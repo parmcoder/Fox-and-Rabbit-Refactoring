6b5a6ad9fd23bb820d6efefc7e13e08d8f416907
parmcoder <possawat2017@hotmail.com>
2020-07-22 02:58:12 +0700

    change colour for visual aids

--- fox-and-rabbit-v1/src/main/java/io/muic/ooc/fab/entities/AnimalSpecies.java
+++ fox-and-rabbit-v1/src/main/java/io/muic/ooc/fab/entities/AnimalSpecies.java
@@ -4,10 +4,10 @@ import java.awt.*;
 
 public enum AnimalSpecies {
 
-    RABBIT(0.1, Rabbit.class, Color.CYAN, 9),
-    FOX(0.03, Fox.class, Color.ORANGE, 15),
-    TIGER(0.01, Tiger.class, Color.BLACK, 60),
-    HUNTER(0.0009, Hunter.class, Color.RED, 0);
+    RABBIT(0.1, Rabbit.class, Color.GREEN, 8),
+    FOX(0.1, Fox.class, Color.PINK, 20),
+    TIGER(0.03, Tiger.class, Color.RED, 40),
+    HUNTER(0.0009, Hunter.class, Color.BLACK, 0);
 
     private double breedingProbability;
 
